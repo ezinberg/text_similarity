b1e8be371380c783b0df8bb6ef1176ee0da72b64
Ezra Zinberg <33340285+ezinberg@users.noreply.github.com>
2021-02-06 09:41:03 -0500

    Update similarity.py
    added comments detailing similarity metric

--- similarity.py
+++ similarity.py
@@ -1,11 +1,21 @@
-# Ezra Zinberg
-
 # text similarity
 
 def similarity(t1, t2):
 
+  # metric:
+  # delta_u := number of unigrams that are included in one text and missing in the
+  #   other text
+  # delta_b := number of bigrams that are included in one text and missing in the
+  #   other text
+  # total_u := total number of unigrams in both texts
+  # total_b := total number of bigrams in both texts
+  # U (unigrams) = 1 - delta_u / total_u
+  # B (bigrams) = 1 - delta_b / total_b
+  # Similarity = (2*U + B) / 3
+
+
   # define a few stopwords
-  stops = ["the", "it", "i", "you", "he", "she", "him", "her",
+  stops = ["the", "it", "i", "you", "he", "she", "him", "her", 
            "a", "in", "or", "and", "", " "]
 
   # UNIGRAMS
@@ -18,13 +28,13 @@ def similarity(t1, t2):
   words1 = [word for word in words1 if word not in stops]
 
   # contains unigram counts for words
-  unigrams1 = {}
+  unigrams1 = {} 
   for word in words1:
     if word in unigrams1:
       unigrams1[word] = unigrams1[word] + 1
     else:
       unigrams1[word] = 1
-
+   
   # preprocess t2
   t2 = t2.lower()
   no_punc2 = t2.replace('.', '').replace('\n', '')
@@ -32,7 +42,7 @@ def similarity(t1, t2):
   words2 = [word for word in words2 if word not in stops]
 
   # contains unigram counts for words
-  unigrams2 = {}
+  unigrams2 = {} 
   for word in words2:
     if word in unigrams2:
       unigrams2[word] = unigrams2[word] + 1
@@ -114,41 +124,42 @@ def similarity(t1, t2):
   unigram_score = 1 - (diff_sum / total_unique_count)
   bigram_score = 1 - (bi_diff_sum / bi_total_unique_count)
 
-
   # weighted average of uni and bigram scores to create overall similarity score
   return (unigram_score*2 + bigram_score) / 3
+
+
 
 def main():
 
   # sample text
 
-  s1 = '''The easiest way to earn points with Fetch Rewards is to just shop for
-  the products you already love. If you have any participating brands on your
+  s1 = '''The easiest way to earn points with Fetch Rewards is to just shop for 
+  the products you already love. If you have any participating brands on your 
   receipt, you'll get points based on the cost of the products. You don't need
-  to clip any coupons or scan individual barcodes. Just scan each grocery
+  to clip any coupons or scan individual barcodes. Just scan each grocery 
   receipt after you shop and we'll find the savings for you.'''
-
+  
   s2 = '''The easiest way to earn points with Fetch Rewards is to just shop for
-   the items you already buy. If you have any eligible brands on your receipt,
-   you will get points based on the total cost of the products. You do not need
-   to cut out any coupons or scan individual UPCs. Just scan your receipt after
+   the items you already buy. If you have any eligible brands on your receipt, 
+   you will get points based on the total cost of the products. You do not need 
+   to cut out any coupons or scan individual UPCs. Just scan your receipt after 
    you check out and we will find the savings for you.'''
-
-  s3 = '''We are always looking for opportunities for you to earn more points,
-  which is why we also give you a selection of Special Offers. These Special
-  Offers are opportunities to earn bonus points on top of the regular points
+  
+  s3 = '''We are always looking for opportunities for you to earn more points, 
+  which is why we also give you a selection of Special Offers. These Special 
+  Offers are opportunities to earn bonus points on top of the regular points 
   you earn every time you purchase a participating brand. No need to pre-select
-  these offers, we'll give you the points whether or not you knew about the
+  these offers, we'll give you the points whether or not you knew about the 
   offer. We just think it is easier that way.'''
-
+  
   # identical to s3
-  s4 = '''We are always looking for opportunities for you to earn more points,
-  which is why we also give you a selection of Special Offers. These Special
-  Offers are opportunities to earn bonus points on top of the regular points
+  s4 = '''We are always looking for opportunities for you to earn more points, 
+  which is why we also give you a selection of Special Offers. These Special 
+  Offers are opportunities to earn bonus points on top of the regular points 
   you earn every time you purchase a participating brand. No need to pre-select
-  these offers, we'll give you the points whether or not you knew about the
+  these offers, we'll give you the points whether or not you knew about the 
   offer. We just think it is easier that way.'''
-
+  
   # random text
   r1 = '''sdlfkjsf ruhvek ebke skufvh weub e dfvub skduhf'''
   r2 = '''xeomxoe wygeyg vkmkj ueueue vkbvk torihjg'''
@@ -160,7 +171,7 @@ def main():
   # reverse (same unigrams, none of the same bigrams)
   rev1 = '''i am writing this sentence to test my program hoping it works'''
   rev2 = '''works it hoping program my test to sentence this writing am i'''
-
+  
 
   print(similarity(s1,s2))
   print(similarity(s2,s3))
